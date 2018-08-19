9b115651bc13cbdac007d12f713e51fac8921bbe
Ashley Bland <ashleyjbland@gmail.com>
2018-08-19 16:12:28 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,7 +10,7 @@ end
 def current_player(board)
   if turn_count(board) == 0 || turn_count(board) % 2
     "X"
-  elsif turn_count(board) == 2
+  elsif turn_count(board) == 1
     "O"
   else
     "O"
