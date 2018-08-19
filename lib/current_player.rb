989e5e9a81446e66a1fb239d378ae634366f46db
Ashley Bland <ashleyjbland@gmail.com>
2018-08-19 16:09:54 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,7 +10,7 @@ end
 def current_player(board)
   if turn_count(board) == 0 || turn_count(board) % 2
     "X"
-  if turn_count(board) != value.even? || turn_count(board) == 3
+  else
     "O"
   end
 end
