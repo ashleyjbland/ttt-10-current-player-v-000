5e249d8fe9ff3d53ff89b9916b25c1e8b9e7a4ef
Ashley Bland <ashleyjbland@gmail.com>
2018-08-19 15:43:36 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,7 @@
 def turn_count(board)
   counter = 0
-  turn = 9 - counter
-  turn. each do |turn|
-    turn == "X" || turn == "O"
+  turn. count do |board|
+    board == "X" || board == "O"
     counter += 1
     puts "#{counter}"
   end
